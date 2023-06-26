ea3ca8746c8e31d11c39a85274990b12b74049be
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-06-26 14:53:33 -0300

    feat: Add Form component

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './styles.css'
+
+const Form = ({ addColor }) => {
+  const [color, setColor] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addColor(color)
+  }
+
+  return (
+    <section className='container'>
+      <h4>Gerador de paleta de cores!</h4>
+      <form className='color-form' onSubmit={ handleSubmit }>
+
+        <input
+          type="color"
+          value={ color }
+          onChange={ (e) => setColor(e.target.value) }
+        />
+
+        <input
+          type="text"
+          value={ color }
+          onChange={ (e) => setColor(e.target.value) }
+          placeholder='#f15025'
+        />
+
+        <button
+          className='btn'
+          type='submit'
+          style={ { background: color } }
+        >
+          Confirmar
+        </button>
+      </form>
+
+    </section>
+  )
+}
+
+export default Form
